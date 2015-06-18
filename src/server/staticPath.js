29796ae274954249a448bee04f37b693e4bdb612
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-06-18 10:39:03 +0200

    h_00

--- src/server/staticPath.js
+++ src/server/staticPath.js
@@ -1,10 +1,10 @@
 import path from 'path';
 
-process.env.PWD = process.cwd();
+process.env.PWD = __dirname);
 console.log('====>', process.env.PWD);
 
 const staticPath = path.resolve(path.join(process.env.PWD , '/public'));
 
-console.log(' static path ===>', staticPath);
+console.log('static path ===>', staticPath);
 
 export default staticPath;
