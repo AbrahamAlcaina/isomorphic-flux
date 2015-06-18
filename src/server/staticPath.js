b6603db6c8d06f974534d359b23dc5eb0ca14d66
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-06-18 11:56:39 +0200

    h_08

--- src/server/staticPath.js
+++ src/server/staticPath.js
@@ -8,10 +8,12 @@ let base = __dirname;
 
 if (process.env.NODE_ENV == 'production') {
     console.log('production');
-    base = process.cwd();
+    //base = process.cwd();
+    base = ''
+    console.log(base);
 }
 
-const staticPath = path.resolve(path.join(base, '/public'));
+const staticPath = path.resolve(path.join(base, 'public'));
 
 console.log('static path ===>', staticPath);
 
