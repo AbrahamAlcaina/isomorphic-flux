8b66c1a70110d5ae043a8928bbc630810f1b7e34
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-06-18 12:13:25 +0200

    h_11

--- src/server/staticPath.js
+++ src/server/staticPath.js
@@ -1,13 +1,11 @@
 import path from 'path';
 
-
-
-
 let staticPath = path.resolve(path.join(__dirname, '../../public'));
 
 console.log(process.env);
 if (process.env.NODE_ENV == 'production') {
     console.log('production');
+    console.log(__dirname);
     //base = process.cwd();
     staticPath = 'public';
 }
