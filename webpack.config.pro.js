c8ed3fc24b48c618c4dc7dab409a08f06ea09d93
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-07-03 14:04:36 +0200

    improve compresion

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -4,7 +4,16 @@ var config = require('./webpack.config.js');
 config.watch = false;
 config.plugins = [
     new webpack.optimize.UglifyJsPlugin({
-        minimize: true, output: {comments: false}
+        minimize: true, output: {comments: false}, mangle: true, compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true
+    }
     }),
     new ExtractTextPlugin("css/[name].css")
 ]
